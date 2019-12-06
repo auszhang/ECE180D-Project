6d4a1eeb9f09c7fe698e3f32ea0e0358550c9c1e
Austin Zhang <aus.zhang@gmail.com>
2019-12-05 18:34:02 -0800

    ui cleanup part 2 - first coach mark

--- CapApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ CapApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,18 +74,18 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080023;
-        public static final int end = 0x7f080043;
-        public static final int gone = 0x7f080051;
-        public static final int invisible = 0x7f08005b;
-        public static final int left = 0x7f080061;
-        public static final int packed = 0x7f080076;
-        public static final int parent = 0x7f080078;
-        public static final int percent = 0x7f08007b;
-        public static final int right = 0x7f080080;
-        public static final int spread = 0x7f0800a3;
-        public static final int spread_inside = 0x7f0800a4;
-        public static final int start = 0x7f0800a9;
-        public static final int top = 0x7f0800c1;
+        public static final int end = 0x7f080045;
+        public static final int gone = 0x7f080053;
+        public static final int invisible = 0x7f08005d;
+        public static final int left = 0x7f080062;
+        public static final int packed = 0x7f080077;
+        public static final int parent = 0x7f080079;
+        public static final int percent = 0x7f08007c;
+        public static final int right = 0x7f080081;
+        public static final int spread = 0x7f0800a4;
+        public static final int spread_inside = 0x7f0800a5;
+        public static final int start = 0x7f0800aa;
+        public static final int top = 0x7f0800c2;
         public static final int wrap = 0x7f0800d3;
     }
     public static final class styleable {
