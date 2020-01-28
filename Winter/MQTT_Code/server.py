fd3c60f8ade2ffcc840db53b8f5fdd056b5c4deb
unknown <michelleruodu@gmail.com>
2020-01-28 12:48:46 -0800

    Server for tracking game state

--- Winter/MQTT_Code/server.py
+++ Winter/MQTT_Code/server.py
@@ -9,7 +9,11 @@ send_path = "topic/serene"
 listen_path = "topic/init_loc"
 rec_client_strings = {}
 MIN_CLIENTS = 1
-cycle = 0
+
+# Variables for tracking game state
+game_grid = None
+potato_row = -1
+potato_col = -1
 
 def on_publish(client,userdata,result):
 	print("LED sequence sent")
@@ -27,47 +31,29 @@ def on_message(client, userdata, msg):
         dead_client = statement[:-4]
         if dead_client in rec_client_strings:
             del rec_client_strings[dead_client]
+    elif "PASS_POTATO" in statement:
+        new_row, new_col, valid = parse_pass(statement, game_grid, potato_row, potato_col)
+        if valid:
+            potato_row = new_row
+            potato_col = new_col
+            pass_msg = string(game_grid[potato_row][potato_col]) + ";RECEIVE"
+            # Send updated potato position to clients
+            client.publish(send_path, pass_msg)
     else:
         split_string = statement.split(";")
         # Map statement to client ID. This overwrites any previous statements from same client.
         rec_client_strings[split_string[4]]=statement
-        
         time.sleep(2)
-
-def assign_lighting(grid, cycle, num_clients):
-    msg = ""
-    if num_clients == 1:
-        msg = steady_on(grid, cycle)
-        #print("steady_on")
-    elif (num_clients == 2):
-        msg = flash_slow(grid, cycle)
-        #print("flash_slow")
-    elif num_clients == 3:
-        #msg = flash_fast(grid, cycle)
-        #print("flash_fast")
-        print('three connected')
-        msg = three_connected_colors(grid,cycle, "b", "d")
-        # msg = three_connected_basic(grid,cycle)
-    elif num_clients == 4:
-        msg = UCLA_light_scheme(grid, cycle)
-        #print("UCLA_light_scheme")
-    return msg
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_publish = on_publish
 client.connect(MQTT_SERVER, 1883, 60)
-#client.loop_forever()
 client.loop_start()
 while True:
     time.sleep(2)
     if len(rec_client_strings) >= MIN_CLIENTS:
         client_data = parse_from_strings_hash(rec_client_strings)
         print(client_data)
-        grid, _ = localize_all(client_data)
-        light_asgns = assign_lighting(grid, cycle, len(client_data))
-        print("THIS IS THE LIGHT ASSIGNMENT")
-        print(light_asgns)
-        client.publish(send_path, light_asgns)
-        cycle += 1
+        game_grid, _ = localize_all(client_data)
--- Winter/MQTT_Code/server_helpers.py
+++ Winter/MQTT_Code/server_helpers.py
@@ -17,26 +17,29 @@ def parse_from_strings_hash(H):
         data.append(parse_from_string(H[key]))
     return data
 
+def pos_to_row_col(pos):
+    if pos==1:
+        return 0, 0
+    elif pos==2:
+        return 0, 1
+    elif pos==3:
+        return 1, 0
+    elif pos==4:
+        return 1, 1
+    return -1, -1
+
 # localize maps clients to their position in the display grid.
 def localize(data, grid, name_grid):
-    row = int(data[0])
-    col = int(data[1])
-    name = data[2]
-    neighbor = data[3]
-    client_id = int(data[4])
-    if row > grid.shape[0]:
-        padding = np.zeros((row-grid.shape[0], grid.shape[1]))
-        grid = np.concatenate((grid,padding), axis=0)
-        
-        padding = np.empty((row-name_grid.shape[0], name_grid.shape[1]), dtype=object)
-        name_grid = np.concatenate((name_grid,padding), axis=0)
-    if col > grid.shape[1]:
-        padding = np.zeros((grid.shape[0], col-grid.shape[1]))
-        grid = np.concatenate((grid,padding), axis=1)
-        
-        padding = np.empty((name_grid.shape[0], col-name_grid.shape[1]), dtype=object)
-        name_grid = np.concatenate((name_grid,padding), axis=1)
-
+    # Parse data
+    pos = int(data[0]) # pos is an integer 1, 2, 3, 4
+    name = data[1]
+    client_id = int(data[2])
+    row, col = pos_to_row_col(pos)
+    
+    # Check if invalid position
+    if row == -1 || col == -1:
+        return grid, name_grid, False
+    
     if grid[row-1][col-1] != 0:
         # unsuccessful if another client is already in this position
         return grid, name_grid, False
@@ -47,14 +50,13 @@ def localize(data, grid, name_grid):
         # set client to this position
         grid[row-1][col-1] = client_id
         name_grid[row-1][col-1] = name
-        # set client's neighbor's name
-        # if (col-1)!=0:
-            # name_grid[row-1][col-2] = neighbor
     return grid, name_grid, True
 
+# Returns grid (client id to position) and name_grid (client name to position)
 def localize_all(data_array):
-    grid = np.zeros((1,1))
-    name_grid = np.empty((1,1), dtype=object)
+    # Initialize 2x2 grid
+    grid = np.zeros((2,2))
+    name_grid = np.empty((2,2), dtype=object)
     to_place = []
     for data in data_array:
         grid, name_grid, successful = localize(data,grid,name_grid)
@@ -62,3 +64,63 @@ def localize_all(data_array):
             to_place.append(data)
     # Do something with misplaced clients
     return grid, name_grid
+
+# Return row and col number of the client, given client id.
+def find_client_in_grid(client_id, grid):
+    if grid[0][0] == client_id:
+        return 0, 0
+    elif grid[0][1] == client_id:
+        return 0, 1
+    elif grid[1][0] == client_id:
+        return 1, 0
+    elif grid[1][1] == cliend_id:
+        return 1, 1
+    return -1, -1
+
+# Given 0 return 1. Else return 0.
+def flip_num(num):
+    if num == 0:
+        return 1
+    else:
+        return 0
+
+# Given starting position + direction, return new position
+def get_pos_from_dir(row, col, direction):
+    new_row = -1
+    new_col = -1
+    if direction == "RIGHT":
+        new_row = flip_num(col)
+        new_col = row
+    elif direction == "LEFT":
+        new_row = col
+        new_col = flip_num(row)
+    elif direction == "ACROSS":
+        new_row = flip_num(row)
+        new_col = flip_num(col)
+    return new_row, new_col
+
+# Given client statement, game grid, and current potato position
+# Return new position of potato and whether move is valid
+def parse_pass(statement, game_grid, potato_row, potato_col):
+    data = parse_from_string(statement)
+    client_id = int(data[0])
+    direction = data[2]
+    c_row, c_col = find_client_in_grid(client_id, game_grid)
+    
+    # Invalid if client is not in the game
+    if c_row == -1 || c_col == -1:
+        return -1, -1, False
+    
+    # Invalid if client trying to pass does not have potato
+    if c_row != potato_row || c_col != potato_col:
+        return -1, -1, False
+    
+    # Get position of receiver
+    new_p_row, new_p_col = get_pos_from_dir(c_row, c_col, direction)
+    
+    # Invalid if no client at receiving position
+    if game_grid[new_p_row][new_p_col] == 0:
+        return -1, -1, False
+    
+    return new_p_row, new_p_col, True
+    
