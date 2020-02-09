7143aec3e1de8605b5e24ffb84a38c59266240d7
Kenny Chan <kennych418@ucla.edu>
2020-02-08 23:55:14 -0800

    Integrated gesture recognition

--- Winter/IMU_Code/GestureRecognition.py
+++ Winter/IMU_Code/GestureRecognition.py
@@ -2,6 +2,13 @@ import MotionTracker
 
 def read():
     output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    output = MotionTracker.readIMU(MotionTracker.IMU)
+    
     ACCx = output[0]
     ACCy = output[1]
     ACCz = output[2]
@@ -9,9 +16,11 @@ def read():
     CFy = output[4]
 
     if ACCx > 2500:         #Across
-        return "Across"
+        return "A"
     elif ACCz > 2000:       #Left
-        return "Left"
+        return "L"
     elif ACCy < -1000:      #Right
-        return "Right"
+        return "R"
+    else:
+        return "X"
     
