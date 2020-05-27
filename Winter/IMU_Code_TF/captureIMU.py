c2edd9b08b4df0f707de0b282689815664a692fc
Kenny Chan <chankenny900@gmail.com>
2020-05-26 20:28:59 -0700

    TF done

--- Winter/IMU_Code_TF/captureIMU.py
+++ Winter/IMU_Code_TF/captureIMU.py
@@ -1,4 +1,9 @@
 import MotionTrackerNN
+import RPi.GPIO as GPIO 
+
+#setup vibration motor
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
 
 def main():
     accelerationThreshold = 600
@@ -9,6 +14,7 @@ def main():
 
     while True:
         while samplesRead == numSamples: 
+            GPIO.output(12,1)
             output = MotionTrackerNN.readIMU(MotionTrackerNN.IMU)
             aSum = abs(output[0]) + abs(output[1]) + abs(output[2])
             if aSum >= accelerationThreshold:
@@ -16,6 +22,7 @@ def main():
                 break
         
         while samplesRead < numSamples:
+            GPIO.output(12,0)
             output = MotionTrackerNN.readIMU(MotionTrackerNN.IMU)
             samplesRead = samplesRead + 1          
             print("%f,%f,%f,%f,%f,%f" %(output[0], output[1], output[2], output[3], output[4], output[5]))
